9805de7c9150ffed557041d019cd2127acade352
Ajai <shadurshan02@gmail.com>
2021-10-13 22:04:19 +0530

    Environment Sprites Added
    Environment Sprites (PSD) Added.
    Dummy Player and script created,

---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
+    }
+}
