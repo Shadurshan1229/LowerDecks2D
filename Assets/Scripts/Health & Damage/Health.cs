b4d1d46c35c96d2eeafe01a7907ba3d3efc001a0
Ajai <shadurshan02@gmail.com>
2021-10-15 15:05:37 +0530

    Prefabs Updated and Bug fixing

--- Assets/Scripts/Health & Damage/Health.cs
+++ Assets/Scripts/Health & Damage/Health.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
-
     private Animator anim;
-
     private bool dead;
+
+    [Header("iFrame")]
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private int numberofFlashes;
+    private SpriteRenderer spriteRenderer;
+
 
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -24,7 +30,9 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("Hurt");
+
             //iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -42,5 +50,20 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+
+        //Invulnerability Duration
+        for (int i = 0; i < numberofFlashes; i++)
+        {
+            spriteRenderer.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFrameDuration/(numberofFlashes *3));
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(iFrameDuration / (numberofFlashes * 3));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
     }
 }
