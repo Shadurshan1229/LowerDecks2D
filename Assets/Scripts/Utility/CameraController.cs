8efa965e51b1c877aac546d172a0c7384407af2c
Ajai <shadurshan02@gmail.com>
2021-10-15 13:12:21 +0530

    Game Mechanics Update
    Heath System added
    Land Mine enemy added
    In game Heath UI added

--- Assets/Scripts/Utility/CameraController.cs
+++ Assets/Scripts/Utility/CameraController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //Room camera
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    //Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float cameraHeight;
+
+    private float lookAhead;
+
     // Update is called once per frame
     void Update()
     {
-        
+        //Room camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        transform.position = new Vector3(player.position.x + lookAhead, player.position.y + cameraHeight, transform.position.z);
+
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+
+    public void MovetoNewSector(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
     }
 }
---
+++ Assets/Scripts/Utility/Door.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Transform previousSector;
+    [SerializeField] private Transform nextSector;
+    [SerializeField] private CameraController cam;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x < transform.position.x)
+                cam.MovetoNewSector(nextSector);
+        }
+    }
+}
